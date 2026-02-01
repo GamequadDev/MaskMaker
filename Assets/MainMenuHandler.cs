93040d28721f9d3fc107b8e80bf9619e4c5f4cdc
Sebastian Masternak <76049469+sebastianmasternak@users.noreply.github.com>
2026-02-01 16:52:01 +0100

    Update

--- Assets/MainMenuHandler.cs
+++ Assets/MainMenuHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour
@@ -22,7 +23,7 @@ public class MainMenuHandler : MonoBehaviour
 
     public void StartGameOnClick()
     {
-        // Zmiana sceny na scenę gry
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void CreditsOnClick()
