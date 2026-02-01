8731e66fecb5d9a62457b71cf5066ce4029b0fe4
mw <gamequad@gmail.com>
2026-02-01 13:23:21 +0100

    Linux build

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 
 public class BuildScript {
-    static string[] Scenes = { "Assets/Scenes/Store/MainStore.unity" };
+    static string[] Scenes = { "Assets/Scenes/Store/MainStore1.unity" };
 
     public static void BuildWebGL() {
         BuildPipeline.BuildPlayer(Scenes, "Builds/WebGL", BuildTarget.WebGL, BuildOptions.None);
@@ -10,5 +10,10 @@ public class BuildScript {
     public static void BuildWindows() {
         // Zauważ, że tu podajemy plik .exe, a nie tylko folder
         BuildPipeline.BuildPlayer(Scenes, "Builds/Windows/MojaGra.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+
+    public static void BuildLinux() {
+        // Dla Linuxa buildem jest plik wykonywalny (zazwyczaj bez rozszerzenia lub .x86_64)
+        BuildPipeline.BuildPlayer(Scenes, "Builds/Linux/MojaGra.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
     }
 }
